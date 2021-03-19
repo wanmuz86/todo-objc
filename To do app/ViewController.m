5fc27559acf11f84a3f8a9151fcd996d588fce09
Wan Muzaffar <wanmuzaffar@Wans-MacBook-Pro.local>
2021-03-19 09:16:11 +0800

    add item

--- To do app/ViewController.m
+++ To do app/ViewController.m
@@ -7,7 +7,10 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController () <UITableViewDataSource, UITableViewDelegate>
+
+@property (strong, nonatomic) NSMutableArray *todos;
+@property (weak, nonatomic) IBOutlet UITableView *tableView;
 
 @end
 
@@ -15,8 +18,45 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    self.todos = @[
+    @{@"name":@"Makan",
+      @"tempat": @"Food court"},
+    @{@"name":@"Beli croissant",
+      @"tempat":@"Baker Cottage"},
+    @{@"name":@"Tengok Xiamoi",
+      @"tempat":@"Xiaomi store"},
+    @{@"name":@"Go home",
+      @"tempat":@"Serdang"}
+    ];
+
+
+    
     // Do any additional setup after loading the view.
 }
 
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
+
+    UITableViewCell* cell = [tableView dequeueReusableCellWithIdentifier:@"Cell" forIndexPath:indexPath];
+    cell.textLabel.text = self.todos[indexPath.row][@"name"];
+    cell.detailTextLabel.text =
+    self.todos[indexPath.row][@"tempat"];
+    return cell;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+  
+    return self.todos.count;
+}
+
+- (IBAction)addPressed:(id)sender {
+    
+    UIAlertController* alertCtrl = [UIAlertController alertControllerWithTitle:@"Hello World" message:@"Hello jom break" preferredStyle:UIAlertControllerStyleAlert];
+    UIAlertAction* okAction =
+    [UIAlertAction actionWithTitle:@"OK" style:UIAlertActionStyleDefault handler:nil];
+    [alertCtrl addAction:okAction];
+    [self presentViewController:alertCtrl animated:YES completion:nil];
+    
+}
 
 @end
